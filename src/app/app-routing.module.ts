42e66ba6f592b959f67249d4ca00b24796157052
Sumit_growpital <sumit@growpital.com>
2023-07-03 18:37:33 +0530

    feat: routing for basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './My-Components/home/home.component';
+import { SellerComponent } from './My-Components/seller/seller.component';
+import { LoginComponent } from './My-Components/login/login.component';
+import { CartComponent } from './My-Components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'seller-auth', component: SellerComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'cart', component: CartComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './My-Components/header/header.component';
+import { SellerComponent } from './My-Components/seller/seller.component';
+import { HomeComponent } from './My-Components/home/home.component';
+import { LoginComponent } from './My-Components/login/login.component';
+import { CartComponent } from './My-Components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SellerComponent,
+    HomeComponent,
+    LoginComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
